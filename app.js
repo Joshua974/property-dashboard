db4a4e787b6769b91766a3892fa90e8531342419
Joshua Rufus <joshuarufus055@gmail.com>
2026-03-16 15:20:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const supabaseUrl = "https://zlcqbtecztppsikhmnrl.supabase.co"
+
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsY3FidGVjenRwcHNpa2htbnJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNTE5NTEsImV4cCI6MjA4ODkyNzk1MX0.VMPl1Zijb5YU8v9ecXsYVMRFbG9S6PuIds6uek8ThRI"
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
@@ -7,15 +8,20 @@ const input = document.getElementById("search")
 
 input.addEventListener("keyup", async () => {
 
-const query = input.value
+const query = input.value.trim()
+
+if(query.length === 0){
+document.getElementById("results").innerHTML=""
+return
+}
 
 const { data, error } = await supabase
 .from("tenant_overview")
 .select("*")
 .ilike("full_name", `%${query}%`)
 
-console.log("DATA:", data)
-console.log("ERROR:", error)
+console.log(data)
+console.log(error)
 
 display(data)
 
@@ -23,17 +29,18 @@ display(data)
 
 function display(data){
 
-const div = document.getElementById("results")
-div.innerHTML=""
+const container = document.getElementById("results")
+
+container.innerHTML=""
 
 if(!data || data.length === 0){
-div.innerHTML = "No results found"
+container.innerHTML="No results found"
 return
 }
 
 data.forEach(t=>{
 
-div.innerHTML += `
+container.innerHTML += `
 <div class="card">
 
 <h3>${t.full_name}</h3>
@@ -41,11 +48,10 @@ div.innerHTML += `
 Phone: ${t.phone}<br>
 Email: ${t.email}<br>
 
-Property: ${t.property_name}<br>
+Property: ${t.property_name}
 
 </div>
 `
-
 })
 
 }
